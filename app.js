c015af7cb6f087b5e663a721d5ecab910747c367
redolf <redolf639@gmail.com>
2022-09-26 09:33:09 +0000

    all pages created, array push working now and populates on the home ejs page

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
 const PORT = process.env.PORT || 3000; 
 
 const app = express();
@@ -12,21 +11,46 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const homeStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam doloremque deserunt, exercitationem reiciendis a accusamus? Non illo minima cum modi rem totam fugit animi dolorum. Totam tempora consectetur nulla expedita ex alias minima nam eaque, consequuntur enim beatae explicabo hic perspiciatis, quae quasi. Consequuntur, aperiam a! Eligendi tenetur molestias, dolorem omnis exercitationem natus ipsa commodi quos explicabo iure impedit hic voluptatibus adipisci, corrupti voluptatem blanditiis voluptate quasi dolore! Ipsum earum maiores voluptas veritatis accusamus delectus voluptatem illum error blanditiis, porro veniam doloribus, repellat quam dolorem quis vel expedita quas perferendis deserunt fugiat vitae quasi totam esse repellendus? Nulla, quos recusandae.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "This is the Home page. iLorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam doloremque deserunt, exercitationem reiciendis a accusamus? Non illo minima cum modi rem totam fugit animi dolorum. Totam tempora consectetur nulla expedita ex alias minima nam eaque, consequuntur enim beatae explicabo hic perspiciatis, quae quasi. Consequuntur, aperiam a! Eligendi tenetur molestias, dolorem omnis exercitationem natus ipsa commodi quos explicabo iure impedit hic voluptatibus adipisci, corrupti voluptatem blanditiis voluptate quasi dolore! Ipsum earum maiores voluptas veritatis accusamus delectus voluptatem illum error blanditiis, porro veniam doloribus, repellat quam dolorem quis vel expedita quas perferendis deserunt fugiat vitae quasi totam esse repellendus? Nulla, quos recusandae.";
+const aboutContent = "This is the about us page. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "This is the contact us page. Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-
-
+//variables | arrays
+let posts = [];
 
 app.get("/",function(req,res){
-    res.render("home", {StartingContent: homeStartingContent}); //on the root route, the home page is rendered; 
+    res.render("home", {
+      StartingContentHome: homeStartingContent,
+      posts: posts //array posts and posts from forEach in home.ejs accordingly; 
+    
+    }); //on the root route, the home page is rendered;  
+     
 });
 
+app.get("/about",function(req,res){
+    res.render("about", {StartingContentAbout: aboutContent}); //on the about route, the about page is rendered; 
+});
 
+app.get("/contact", function(req,res){
+    res.render("contact", {StartingContentContact: contactContent}); 
+})
 
+app.get("/compose", (req,res)=> {
+    res.render("compose"); 
+});
 
+app.post("/compose", (req,res)=> {
+  const post = {
+    title: req.body.postTitleCompose,
+    content: req.body.postMsgCompose
+    
+  };
+  
+  posts.push(post);
 
+  res.redirect("/"); 
+
+});
 
 
 
